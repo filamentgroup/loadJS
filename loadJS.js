c468c26510db51b42a9719efa4a305c7fa9235a9
Scott Jehl <scottjehl@gmail.com>
2014-07-10 09:13:27 -0500

    window ref

--- loadJS.js
+++ loadJS.js
@@ -1,7 +1,7 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
 function loadJS( src ){
 	"use strict";
-	var ref = document.getElementsByTagName( "script" )[ 0 ];
+	var ref = window.document.getElementsByTagName( "script" )[ 0 ];
 	var script = window.document.createElement( "script" );
 	script.src = src;
 	ref.parentNode.insertBefore( script, ref );
