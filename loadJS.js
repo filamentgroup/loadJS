4e22165e4c5dadd9c16c323405fd19e79375fae0
stuartsantos <santos.stuart@gmail.com>
2014-08-11 11:16:59 +0100

    add iteration and order
    added the ability to iterate on multiple files and load the js in a
    predictable order so that the files can have dependencies

--- loadJS.js
+++ loadJS.js
@@ -1,9 +1,14 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
-function loadJS( src ){
-	"use strict";
-	var ref = window.document.getElementsByTagName( "script" )[ 0 ];
-	var script = window.document.createElement( "script" );
-	script.src = src;
-	ref.parentNode.insertBefore( script, ref );
-	return script;
+function loadJS(){
+	var src = ["path/to/first/script.js","path/to/second/script.js"]; //can add multiple js paths here comma delimited
+	src.reverse(); //reverses the order so that when added to the DOM, they are in the proper order
+	for (var i = 0; i < src.length; i++) { //iterates on each path in the array
+		"use strict";
+		var ref = window.document.getElementsByTagName( "script" )[ 0 ];
+		var script = window.document.createElement( "script" );
+		script.src = src[i];
+		ref.parentNode.insertBefore( script, ref );
+	};
 }
+
+loadJS();
