17824a7129a693eebb66129d1540e0d23e1b78ea
Tim Kadlec <tim@timkadlec.com>
2014-09-22 13:04:33 -0500

    Adding callback capabilities to loadJS

--- loadJS.js
+++ loadJS.js
@@ -1,9 +1,17 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
-function loadJS( src ){
+function loadJS( src, cb ){
 	"use strict";
 	var ref = window.document.getElementsByTagName( "script" )[ 0 ];
 	var script = window.document.createElement( "script" );
 	script.src = src;
 	ref.parentNode.insertBefore( script, ref );
+	if (cb) {
+		script.onload = function(){
+			try {
+				cb();
+			}
+			catch (e) {}
+		};
+	}
 	return script;
 }
