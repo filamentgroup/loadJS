45e41fb23d5d59dd4328fc225d7d3a5d71dbd6b9
Scott Jehl <scott@filamentgroup.com>
2014-10-15 11:58:20 -0500

    In the latest Chrome release, it appears that scripts loaded dynamically without an async attr will now block rendering. This change restores the non-blocking behavior that was present before the latest chrome update.

--- loadJS.js
+++ loadJS.js
@@ -4,6 +4,7 @@ function loadJS( src, cb ){
 	var ref = window.document.getElementsByTagName( "script" )[ 0 ];
 	var script = window.document.createElement( "script" );
 	script.src = src;
+	script.async = true;
 	ref.parentNode.insertBefore( script, ref );
 	if (cb) {
 		script.onload = cb;
