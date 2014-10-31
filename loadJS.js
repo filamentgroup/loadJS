8c172a41e2bb2f94c4d10d634b25487aa84ef3d5
Jake Boone <jakeboone02@gmail.com>
2014-10-31 09:47:09 -0400

    Use single quotes consistently

--- loadJS.js
+++ loadJS.js
@@ -1,8 +1,8 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
 function loadJS( src, cb ){
-	"use strict";
-	var ref = window.document.getElementsByTagName( "script" )[ 0 ];
-	var script = window.document.createElement( "script" );
+	'use strict';
+	var ref = window.document.getElementsByTagName( 'script' )[ 0 ];
+	var script = window.document.createElement( 'script' );
 	script.src = src;
 	script.async = true;
 	ref.parentNode.insertBefore( script, ref );
