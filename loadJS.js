d2751a875a8a07247836a5b0351020a585f2209c
Valera Trubachev <vetruvet@gmail.com>
2015-02-24 10:57:36 -0600

    Add error callback
    Add error callback if script fails to load

--- loadJS.js
+++ loadJS.js
@@ -1,5 +1,5 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
-function loadJS( src, cb ){
+function loadJS( src, cb, errorcb ){
 	"use strict";
 	var ref = window.document.getElementsByTagName( "script" )[ 0 ];
 	var script = window.document.createElement( "script" );
@@ -8,6 +8,9 @@ function loadJS( src, cb ){
 	ref.parentNode.insertBefore( script, ref );
 	if (cb && typeof(cb) === "function") {
 		script.onload = cb;
+	}
+	if (errorcb && typeof(errorcb) === "function") {
+		script.onerror = errorcb;
 	}
 	return script;
 }
