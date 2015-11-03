de294c2839c468ee85dc9f74258b4bcb17f137c7
Scott Jehl <scott@filamentgroup.com>
2015-11-03 15:55:55 -0600

    Only expose loadJS on the global object if module is undefined. Play more nicely with commonjs

--- loadJS.js
+++ loadJS.js
@@ -1,9 +1,9 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
 (function( w ){
-	w.loadJS = function( src, cb ){
+	var loadJS = function( src, cb ){
 		"use strict";
-		var ref = window.document.getElementsByTagName( "script" )[ 0 ];
-		var script = window.document.createElement( "script" );
+		var ref = w.document.getElementsByTagName( "script" )[ 0 ];
+		var script = w.document.createElement( "script" );
 		script.src = src;
 		script.async = true;
 		ref.parentNode.insertBefore( script, ref );
@@ -14,6 +14,9 @@
 	};
 	// commonjs
 	if( typeof module !== "undefined" ){
-		module.exports = w.loadJS;
+		module.exports = loadJS;
+	}
+	else {
+		w.loadJS = loadJS;
 	}
 }( typeof global !== "undefined" ? global : this ));
